29aae9b5cb5bade1341eb4206d7d8ae88e7633d7
Sundara Ram Matta <msram0210@gmail.com>
2022-08-22 23:48:40 -0500

    creating the rafce components

--- reactjs-tailwind/src/App.jsx
+++ reactjs-tailwind/src/App.jsx
@@ -1,31 +1,32 @@
 // import React from 'react'
 import styles from './styles'
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
 
 const App = () => (
   // if rfc returns jsx, we can remove the return statement
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
     </div>
